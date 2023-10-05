ff5aa2367cd09794267ad65b66993ed68be23e82
Kroshiks <133707810+Kroshiks@users.noreply.github.com>
2023-10-06 00:56:30 +0300

    доработал код

--- LABA3/LABA3/Source.cpp
+++ LABA3/LABA3/Source.cpp
@@ -11,21 +11,20 @@ struct node
 	int prir;
 };
 
+int start = 1;
 struct node* head = NULL, * last = NULL, * f = NULL;
 int dlinna = 0;
 
-struct node* get_struct(int pr)
+struct node* get_struct(int pr, char s[256])
 {
 	struct node* p = NULL;
-	char s[256];
 
 	if ((p = (struct node*)malloc(sizeof(struct node))) == NULL) // выделяем память под новый элемент списка
 	{
 		printf(" Ошибка при распределении памяти\n");
 		exit(1);
 	}
-	scanf(" %s", &s);
-	if (*s == 0)
+	 if (*s == 0)
 	{
 		printf("Запись не была произведена\n");
 		return NULL;
@@ -36,9 +35,9 @@ struct node* get_struct(int pr)
 	return p; // возвращаем указатель на созданный элемент
 }
 
-void queue(struct node** head, struct node** last)
+void queue(struct node** head, struct node** last, char s[256])
 {
-	struct node* temp = get_struct(0);
+	struct node* temp = get_struct(0,s);
 
 	if (*head == NULL && temp != NULL)
 	{
@@ -52,9 +51,9 @@ void queue(struct node** head, struct node** last)
 	}
 }
 
-void ueue(struct node** head)
+void ueue(struct node** head, char s[256])
 {
-	struct node* temp = get_struct(0);
+	struct node* temp = get_struct(0,s);
 
 	if (temp != NULL)
 	{
@@ -64,20 +63,20 @@ void ueue(struct node** head)
 
 }
 
-void prioritet(struct node** head, int pr)
+void prioritet(struct node** head, int pr, char s[256])
 {
 	struct node* prom = *head;
 
-	struct node* temp = get_struct(pr);
-
-	if (*head == NULL || (*head)->prir >= pr)
+	struct node* temp = get_struct(pr,s);
+ 
+	if (*head == NULL || (*head)->prir > pr)
 	{
 		temp->next = *head;
 		*head = temp;
 	}
-	else
+	else 
 	{
-		while (prom->next != NULL && prom->next->prir < pr)
+		while (prom->next != NULL && prom->next->prir <= pr)
 		{
 			prom = prom->next;
 		}
@@ -99,10 +98,66 @@ void free_queue(struct node* head)
 	}
 }
 
+// Удаление элемента по содержимому. 
+void del(char* name)
+{
+	struct node *struc = head; // указатель, проходящий по списку установлен на началосписка
+	struct node *prev = NULL;// указатель на предшествующий удаляемому элемент
+	int flag = 0; // индикатор отсутствия удаляемого элемента в списке
+	if (head == NULL) // если голова списка равна NULL, то список пуст
+	{
+		printf("Список пуст");
+		return;
+	}
+	if (strcmp(name, struc->inf) == 0) // если удаляемый элемент - первый
+	{
+		flag = 1;
+		head = struc->next; // установливаем голову на следующий элемент
+		free(struc); // удаляем первый элемент
+		struc = head; // устанавливаем указатель для продолжения поиска
+	}
+	else
+	{
+		prev = struc;
+		struc = struc->next;
+	}
+	while (struc) // проход по списку и поиск удаляемого элемента
+	{
+		if (strcmp(name, struc->inf) == 0) // если нашли, то
+		{	
+			flag = 1; // выставляем индикатор
+			if (struc->next) // если найденный элемент не последний в списке
+			{
+				prev->next = struc->next; // меняем указатели
+				free(struc); // удаляем элемент
+				struc = prev->next; // устанавливаем указатель для продолженияпоиска
+
+			}
+			else // если найденный элемент последний в списке
+			{
+				prev->next = NULL; // обнуляем указатель предшествующего элемента
+				free(struc); // удаляем элемент
+				return;
+			}
+		}
+		else // если не нашли, то
+		{
+			prev = struc; // устанавливаем указатели для продолжения поиска
+			struc = struc->next;
+		}
+	}
+	if (flag == 0) // если флаг = 0, значит нужный элемент не найден
+	{
+		printf("Элемент не найден\n");
+		return;
+	}
+}
+
 void main()
 {
-	int f = 0;
-	int i = 0;
+	int f = 0,cs = 0;
+	int i = 0, j=0;
+	char s[256];
 	SetConsoleCP(1251);//Установка языка
 	SetConsoleOutputCP(1251);
 
@@ -122,24 +177,32 @@ void main()
 		{
 			system("cls");
 			int pr = NULL;
+			printf("* - окончание записи данных в приоритетную очередь\n");
+			
 
-			printf("Введите количество элементов в списке:");
-			scanf("%d", &dlinna);
-
-			for (int i = 0; i < dlinna; i++)
+			while (start)
 			{
-				printf("\nВведите приоритет обьекта №%d:", i + 1);
-				scanf("%d", &pr);
-
-				if (pr < 0)
+				do
 				{
-					printf(" Ошибка при записи приоритета\n");
-					_getch();
-					return;
-				}
+					printf("\nВведите приоритет обьекта №%d:", j + 1);
+				    scanf("%d", &pr);
+					cs = 0;
+					if (pr<0)
+					{
+						printf(" Ошибка при записи приоритета\n");
+						cs = 1;
+					}
+				}while (cs);
 
-				printf("Введите название элемента №%d:", i + 1);
-				prioritet(&head, pr);
+				printf("Введите название элемента №%d:", j + 1);
+				scanf(" %s", &s);
+				if (*s == '*')
+				{
+					printf("\nПостроение очереди окончено\n\n");
+					start = 0;
+				}
+				else prioritet(&head, pr, s);
+				j++;
 			}
 
 			printf("\n|  №  |  Элемент  |   Приоритет  |\n");
@@ -154,19 +217,51 @@ void main()
 				current = current->next;
 			}	
 			_getch();
+
+			int d;
+			char k[256];
+			
+			printf("\nДля удаления введите 1: ");
+			scanf("%d",&d);
+			 
+			if(d==1)
+			 {
+				 printf("Введите элемент:");
+				 scanf("%s", &k);
+				 del(k);
+			 }
+			 else return;
+			
+			i = 0;
+			struct node* rent = head;
+
+			while (rent != NULL)
+			{
+				i++;
+				printf("|%-5d|%-11s|%-14d|\n", i, rent->inf, rent->prir);
+				printf("+-----+-----------+--------------+\n");
+				rent = rent->next;
+			}
+			_getch();
 			return;
 		}
 		case 2:
 		{
-			system("cls");
+			system("cls"); 
+			printf("\n* - окончание записи данных в очередь\n");
 
-			printf("Введите количество элементов в очереди:");
-			scanf("%d", &dlinna);
-
-			for (int i = 0; i < dlinna; i++)
+			while (start)
 			{
-				printf("\nВведите название элемента №%d:", i + 1);
-				queue(&head, &last);
+				
+				printf("\nВведите название элемента №%d:", j + 1);
+				scanf(" %s", &s);
+				if (*s == '*')
+				{
+					printf("\nПостроение очереди окончено\n\n");
+					start = 0;
+				}
+				else queue(&head, &last, s);
+				j++;
 			}
 
 			printf("\n|  №  |  Элемент  |\n");
@@ -186,14 +281,20 @@ void main()
 		case 3:
 		{
 			system("cls");
+			printf("* - окончание записи данных в стек\n");
 
-			printf("Введите количество элементов в очереди:");
-			scanf("%d", &dlinna);
-
-			for (int i = 0; i < dlinna; i++)
+			while (start)
 			{
-				printf("\nВведите название элемента №%d:", i + 1);
-				ueue(&head);
+
+				printf("\nВведите название элемента №%d:", j + 1);
+				scanf(" %s", &s);
+				if (*s == '*')
+				{
+					printf("\nПостроение очереди окончено\n\n");
+					start = 0;
+				}
+				else ueue(&head, s);
+				j++;
 			}
 
 			printf("\n|  №  |  Элемент  |\n");
@@ -202,13 +303,12 @@ void main()
 			struct node* rrent = head;
 			while (rrent != NULL)
 			{
-				dlinna--;
-				printf("|%-5d|%-11s|\n", dlinna, rrent->inf);
+				j--;
+				printf("|%-5d|%-11s|\n", j, rrent->inf);
 				printf("+-----+-----------+\n");
 				rrent = rrent->next;
 			}
 			_getch();
-
 			return;
 		}
 		case 4:
